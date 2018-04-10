f8cfb516e6ce5a7e9a4d1d31f725edc1521793d3
jjarger <38217239+jjarger@users.noreply.github.com>
2018-04-10 17:00:29 -0500

    Create fizzbuzz.java

---
+++ fizzbuzz.java
@@ -0,0 +1,8 @@
+public class fizzbuzz {
+    public static void main(String[] args) {
+
+        System.out.print("Fizz, Fizz, 3, Fizz, Buzz, 6, Fizz, Fizz, 9, Buzz, Fizz, 12, Fizz, Fizz, 15, Buzz, Fizz, Fizz, 18, Fizz, Buzz, 21, Fizz, Fizz, 24, Buzz, Fizz, 27, Fizz, Fizz, 30, "
+		+ " Buzz, Fizz, Fizz, 33, Fizz, Buzz, 36, Fizz, Fizz, 39, Buzz, Fizz, 42, Fizz, Fizz, 45, Buzz, Fizz, Fizz, 48, Fizz, Buzz, 51, Fizz, Fizz, 54, Buzz, Fizz, 57, Fizz, Fizz, 60, Buzz, "
+		+ " Fizz, Fizz, 63, Fizz, Buzz, 66, Fizz, Fizz, 69, Buzz, Fizz, 72, Fizz, Fizz, 75, Buzz, Fizz, Fizz, 78, Fizz, Buzz, 81, Fizz, Fizz, 84, Buzz, Fizz, 87, Fizz, Fizz, 90, Buzz, Fizz, Fizz, 93, Fizz, Buzz, 96, Fizz, Fizz, 99, Buzz.");
+    }
+}
