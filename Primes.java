5f4802b59c2ab94d53c8b5846e65fb126cf77317
jjarger <38217239+jjarger@users.noreply.github.com>
2018-04-10 17:11:13 -0500

    Update Primes.java

--- Primes.java
+++ Primes.java
@@ -1,8 +1,6 @@
 public class Primes {
     public static void main(String[] args) {
 
-        Scanner sn = new Scanner(System.in);
-
         System.out.print("First 1000 primes:
 	    2      3      5      7     11     13     17     19     23     29
 	   31     37     41     43     47     53     59     61     67     71 
